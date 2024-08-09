80513e81c7f1454da401f0242edcedc25fd46942
erdo99 <167902213+erdo99@users.noreply.github.com>
2024-08-09 16:53:49 +0300

    güncellendi

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,4 +27,6 @@ export async function createInvoice(formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+
+    revalidatePath('/dashboard/invoices');
     }
