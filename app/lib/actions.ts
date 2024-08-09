78df9f6484c2f9e41c5246e1269d75893d1bb980
erdo99 <167902213+erdo99@users.noreply.github.com>
2024-08-09 16:47:33 +0300

    actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,9 @@
 'use server';
 
 import { z } from "zod";
+import { sql } from '@vercel/postgres';
+
+import { revalidatePath } from 'next/cache';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -13,11 +16,15 @@ const FormSchema = z.object({
   const CreateInvoice = FormSchema.omit({ id: true, date: true });
  
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
-      customerId: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
-    };
-    // Test it out:
-    console.log(rawFormData);
-}
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+      });
+      const amountInCents = amount * 100;
+      const date = new Date().toISOString().split('T')[0];
+      await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+    }
